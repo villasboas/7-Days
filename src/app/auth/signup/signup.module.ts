edfcf7ee2af06a76c655b79aaba494bb1c4bfe54
Gustavo Vilas Boas <gu.boas13@gmail.com>
2020-05-01 14:57:50 -0300

    Add signup form

--- src/app/auth/signup/signup.module.ts
+++ src/app/auth/signup/signup.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -13,8 +13,9 @@ import { SignupPage } from './signup.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    SignupPageRoutingModule
+    ReactiveFormsModule,
+    SignupPageRoutingModule,
   ],
-  declarations: [SignupPage]
+  declarations: [SignupPage],
 })
 export class SignupPageModule {}
--- src/app/auth/signup/signup.page.ts
+++ src/app/auth/signup/signup.page.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.page.html',
   styleUrls: ['./signup.page.scss'],
 })
-export class SignupPage implements OnInit {
+export class SignupPage {
+  /**
+   * Signup Form Group
+   *
+   */
+  public signupForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  /**
+   * Constructor Method
+   *
+   */
+  constructor() {
+    this.signupForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(16),
+      ]),
+      password_confirmation: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(16),
+      ]),
+    });
   }
-
 }
